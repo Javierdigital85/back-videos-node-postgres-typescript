18149c446ec6486209783ccdc25e1165c89cd05c
javiercolodro <javiercolodro@gmail.com>
2024-08-31 16:46:58 -0300

    update database.ts:add dialect postgres

--- src/config/database.ts
+++ src/config/database.ts
@@ -2,7 +2,7 @@ import { Sequelize } from "sequelize-typescript";
 import globalContants from "../conts/globalContants";
 
 const sequelize = new Sequelize({
-  dialect: globalContants.DIALECT,
+  dialect: globalContants.DIALECT || "postgres",
   host: globalContants.HOST,
   port: globalContants.DB_PORT,
   username: globalContants.DB_USERNAME,
