9521854260fcee2b549f6f147655b96f9f129794
javiercolodro <javiercolodro@gmail.com>
2024-09-18 17:27:43 -0300

    fix app.ts: express type

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,4 @@
-import express, { Express } from "express";
+import express, { Application } from "express";
 import routerConfig from "./routes/index";
 import morgan from "morgan";
 import cors from "cors";
@@ -6,18 +6,18 @@ import globalConstants from "./conts/globalContants";
 import cookieParser from "cookie-parser";
 
 const allowedOrigins = ["http://localhost:5173", globalConstants.FRONTEND_URL];
-const apiConfiguration = (app: Express) => {
+const apiConfiguration = (app: Application) => {
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
   app.use(cors({ origin: allowedOrigins, credentials: true }));
   app.use(morgan("tiny"));
   app.use(cookieParser());
 };
-const routerConfiguration = (app: Express) => {
+const routerConfiguration = (app: Application) => {
   app.use("/api", routerConfig());
 };
 
-const app: Express = express();
+const app: Application = express();
 apiConfiguration(app);
 routerConfiguration(app);
 
